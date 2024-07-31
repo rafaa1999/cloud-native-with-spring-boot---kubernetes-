935d52e0e507788b5c61ead1cf50392d633fa8ee
rafaa1999 <raferhouma10@gmail.com>
2024-07-31 08:08:52 +0100

    Test the root endpoint

--- src/test/java/com/rafaa/cloud_service/CloudServiceApplicationTests.java
+++ src/test/java/com/rafaa/cloud_service/CloudServiceApplicationTests.java
@@ -1,13 +1,26 @@
 package com.rafaa.cloud_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CloudServiceApplicationTests {
 
+	@Autowired
+	WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	void when_get_root_then_return_message() {
+		webTestClient
+				.get()
+				.uri("/")
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody(String.class).isEqualTo("Spring Boot on the cloud!");
+
 	}
+
 
 }
